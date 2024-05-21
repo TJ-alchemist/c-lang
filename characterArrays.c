c1451ada5e4e23cfbec56eab9276b74649158a78
TJ-alchemist <tejasvijay09@gmail.com>
2024-05-21 19:00:46 +0530

    modify: characterArrays constants

--- characterArrays.c
+++ characterArrays.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #define MAXLINE 1000 /* Maximum input line length*/
 
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int getline(void);
+void copy(void);
 
 int main() {
   // printf("%d\n", getline("abc", 10));
   int len;
-  int max;
-  char line[MAXLINE];
-  char longest[MAXLINE];
+  extern int max;
+  extern char longest[MAXLINE];
 
   max = 0;
 
-  while ((len = getline(line, MAXLINE)) > 0) {
+  while ((len = getline()) > 0) {
     if (len > max) {
       max = len;
-      copy(longest, line);
+      copy();
     }
   }
 
@@ -28,28 +31,34 @@ int main() {
 }
 
 /* Read a line s and return length*/
-int getline(char s[], int lim) {
+int getline(void) {
   int c, i;
+  extern char line[];
 
-  for (i = 0; i < lim - 1 & (c = getchar()) != EOF & c != '\n'; ++i) {
-    s[i] = c;
+  printf("%c", i);
+  for (i = 0; i < MAXLINE - 1 & (c = getchar()) != EOF & c != '\n'; ++i) {
+    line[i] = c;
   }
 
+  printf("%s", line);
+  printf("\n");
+
   if (c == '\n') {
-    s[i] = c;
+    line[i] = c;
     ++i;
   }
 
-  s[i] = '\0';
+  line[i] = '\0';
 
   return i;
 }
 
-void copy(char to[], char from[]) {
+void copy(void) {
   int i;
+  extern char line[], longest[];
 
   i = 0;
-  while ((to[i] = from[i]) != '\0') {
+  while ((longest[i] = line[i]) != '\0') {
     ++i;
   }
 }
