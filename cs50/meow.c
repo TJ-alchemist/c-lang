fbbff0174d6794332297741c27d7edc7b5b8c915
TJ-alchemist <tejasvijay09@gmail.com>
2024-06-20 17:54:49 +0530

    add meow file

---
+++ cs50/meow.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int i = 3;
+  while (i > 0) {
+    printf("meow\n");
+    i--;
+  }
+
+  int j = 0;
+  while (j < 3) {
+    printf("meow\n");
+    j++;
+  }
+
+  return 0;
+}
